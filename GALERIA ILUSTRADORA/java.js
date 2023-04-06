26b9239314c927ce62d8ecbfe1f73597886f646a
Casandra Gonzalez <nicol.casandra.gonzalez.14@gmail.com>
2023-04-06 15:58:14 -0300

    first commit

--- GALERIA ILUSTRADORA/java.js
+++ GALERIA ILUSTRADORA/java.js
@@ -6,6 +6,6 @@ function telefono(){
     location.href="tel:+34678567876";
 }
 
-function instagram(){
-    window.open('https://www.instagram.com/', '_blank');
+function telefono(){
+    location.href="tel:+34678567876";
 }
